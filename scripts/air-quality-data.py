c08e60b8face06b0cb29185dd2a0f71e964a0c34
Marcia Marques <marcia.marques@alumni.usp.br>
2021-02-14 20:58:45 -0300

    update file names

--- scripts/air-quality-data.py
+++ scripts/air-quality-data.py
@@ -12,10 +12,10 @@ end_date = '01/01/2020'
 # variable = 57  # MP2.5
 # variable = 12  # MP 10
 stations = [95, 72, 99, 91, 85, 270, 96, 63, 103, 83, 73, 64]
-file_names = ['../data/usp', '../data/dpedro', '../data/pinheiros',
-              '../data/ccesar', '../data/mooca', '../data/remedios',
-              '../data/senhora', '../data/santana', '../data/taboao',
-              '../data/ibirapuera', '../data/congonhas', '../data/samaro']
+file_names = ['../data/ciduni', '../data/parquedpedro', '../data/pinheiros',
+              '../data/cerqcesar', '../data/mooca', '../data/margtiete',
+              '../data/nsenhora', '../data/santana', '../data/taboao',
+              '../data/ibirapuera', '../data/congonhas', '../data/santoamaro']
 
 # MP2.5
 for station, file_name in zip(stations, file_names):
